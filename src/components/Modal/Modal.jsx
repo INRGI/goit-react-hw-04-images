8bd69a058e31df3e508016e2a7689708108cb062
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-01-02 12:39:15 +0100

    Modal => hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,43 @@
 import { createPortal } from "react-dom";
-import { Modalka, Overlay } from "./Modal.styled";
+import { Img, Modalka, Overlay } from "./Modal.styled";
 
 import PropTypes from 'prop-types';
-const { Component } = require("react");
+const { useEffect } = require("react");
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component{
-
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-        document.body.style.overflow = 'hidden';
-    };
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-        document.body.style.overflow = 'visible';
-    };
-
-    handleKeyDown = event => {
-        if (event.code === 'Escape') {
-            this.props.onClose();
+const Modal = ({largeImageURL, tags, onClose}) => {
+    
+    useEffect(() => {
+        const handleKeyDown = event => {
+            if (event.code === 'Escape') {
+                onClose();
+            };
         };
-    };
 
-    handleBackdropClick = event => {
+        window.addEventListener('keydown', handleKeyDown);
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+            document.body.style.overflow = 'visible'
+        };
+    },[onClose])
+
+    const handleBackdropClick = event => {
         if (event.currentTarget === event.target) {
-            this.props.onClose();
+            onClose();
         }
     };
 
-    render() {
-        const { largeImageURL, tags } = this.props;
-
-        return createPortal(
-            <Overlay onClick={this.handleBackdropClick}>
-                <Modalka className="modal">
-                    <img src={largeImageURL} alt={tags} />
-                </Modalka>
-            </Overlay>,
-            modalRoot
-        );
-    };
+    return createPortal(
+        <Overlay onClick={handleBackdropClick}>
+            <Modalka className="modal">
+                <Img src={largeImageURL} alt={tags} />
+            </Modalka>
+        </Overlay>,
+        modalRoot
+    );
 };
 
 Modal.propTypes = {
--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -18,3 +18,8 @@ export const Modalka = styled.div`
     max-width: calc(100vw - 48px);
     max-height: calc(100vh - 24px);
 `;
+
+export const Img = styled.img`
+    max-width: calc(100vw - 48px);
+    max-height: calc(100vh - 24px);
+`;
